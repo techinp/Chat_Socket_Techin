b1a248e9c852a73128b494444b80f1105ba6ee1b
Techin Pawantao <techin.pw@gmail.com>
2019-05-24 14:06:07 +0700

    [DONE] Show username online

--- server.js
+++ server.js
@@ -19,29 +19,43 @@ app.get('/chatSocket', (req, res) => {
     res.sendFile(__dirname + '/chatSocket.html');
 });
 
-// let users = {};
-// var userCount = 0;
-// var onlineUser = {};
-// onlineUser.count = 0;
-
 // Socket.io
+
+// Create variable
+let users = {};
+let name = [];
+
 io.on('connection', (socket) => {
 
-    let users = [];
+    let username = {};
+    // let name = [];
 
     // Every socket connection has a unique ID
     console.log('new connection: ' + socket.id);
 
     // Login
     socket.on('login', (data) => {
-        users.name = data;
+
+        username.nickname = data;
+
+        // Keep all username in array
+        socket.nickname = data;
+        name.push(socket.nickname);
+        users.name = name;
+        console.log('name', name);
+
+        // users.name.push(data);
+        // users.name = data; ##
+        // name.push(data);
         socket.join('A Room');
         io.in('A Room').emit('login', {
             from: 'server',
             message: data + ' logged in.'
         });
 
-        updateUser(users.length);
+        console.log('name :', users.name);
+
+        updateUser(users.length, users.name);
         
     });
 
@@ -49,82 +63,18 @@ io.on('connection', (socket) => {
     socket.on('msg', (message) => {
         // Send message to clients
         io.in('A Room').emit('msg', {
-            from: users.name,
+            from: username.nickname,
             message: message
         });
     });
 
-    // io.emit('userOnline', {
-    //     userList: users.length
-    // });
-
-    console.log('users.length :', users.length);
-
     // Disconnect
     socket.on('disconnect', () => {
-        updateUser(users.length);
+        updateUser(users.length, users.name);
     });
-
-
-
-    // User Logged in
-    // socket.on('login', (name) => {
-    //     // Map socket.id to the name
-    //     users.id = socket.id;
-    //     users.name = name;
-
-    //     onlineUser.name = name;
-    //     onlineUser.count = onlineUser.count + 1;
-    //     socket.join('userBox');
-    //     io.to('userBox').emit('userCount', {
-    //         userCount: onlineUser.count
-    //     });
-
-    //     console.log('userCount :', onlineUser.count);    
-
-    //     // Broadcast to everyone else (except the sender).
-    //     // Say that the user has logged in.
-    //     socket.broadcast.emit('msg', {
-    //         from: 'server',
-    //         message: `${name} logged in.`
-    //     });
-    // });
-
-    // Message Recieved
-    // socket.on('msg', (message) => {
-    //     // Broadcast to everyone else (except the sender)
-    //     socket.emit('msg', {
-    //         from: users.name,
-    //         message: message
-    //     });
-    //     // Send back the same message to the sender
-    //     socket.emit('msg', {
-    //         from: users.name,
-    //         message: message
-    //     });
-    //     console.log('users :', users);
-    // });
-
-    // Disconnected
-    // socket.on('disconnect', () => {
-    //     onlineUser.count = onlineUser.count - 1;
-    //     socket.join('userBox');
-    //     io.to('userBox').emit('userCount', {
-    //         userCount: onlineUser.count
-    //     });
-    //     console.log('userCount :', onlineUser.count);
-    //     // Remove the socket.id -> name mapping of this user
-    //     io.emit('disconnect', {
-    //         from: users.name,
-    //         message: users.name + ' has disconnect'
-    //     });
-    //     console.log('disconnect - ' + 'id :' + users.id + '/ name : ' + users.name);
-    //     delete users.id;
-    //     delete users.name;
-    // });
 });
 
-function updateUser(list) {
+function updateUser(list, name) {
     list = 0;
     io.in('A Room').clients((err, clients) => {
         if (err) throw err;
@@ -132,7 +82,8 @@ function updateUser(list) {
         console.log('clients :', clients);
         list = clients.length;
         io.in('A Room').emit('userOnline', {
-            list: clients.length
+            list: clients.length,
+            name: name
         });
     });
 }
